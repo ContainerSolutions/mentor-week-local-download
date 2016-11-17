310b4e9831bde799f6a0033881a6163d429edb46
Maarten Hoogendoorn <maarten@moretea.nl>
2016-11-17 15:44:09 +0100

    Tag images correctly, remove on name conflict.
    Note: this will remove containers from the the host machine, and replace
    them with our own. See the remove-installed-containers.sh script to
    remove these containers.

--- download.sh
+++ download.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 set -e
 
-images =$(curl https://raw.githubusercontent.com/ContainerSolutions/mentor-week-local-download/master/images.txt)
+images=$(curl https://raw.githubusercontent.com/ContainerSolutions/mentor-week-local-download/master/images.txt)
 REPO=$(curl https://raw.githubusercontent.com/ContainerSolutions/mentor-week-local-download/master/local-registry-url.txt)
 echo "downloading images from $REPO"
 
+existing_images=$(docker images --format "{{.Repository}}:{{.Tag}}")
+
 for image in images; do
+  echo "Pulling image $image"
   docker pull $REPO/$image
+  existing_images = $(docker images --format "{{.Repository}}:{{.Tag}}" | grep "^registry")
+
+  for existing_image in existing_images; do
+    docker rmi $existing_image
+  done
+
+  docker tag $REPO/$image $image
 done
